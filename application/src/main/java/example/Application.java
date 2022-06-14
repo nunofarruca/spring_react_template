8d58b47d10c4e6dfea0f773d4aa39ed2aa259f48
Nuno F <nunofarruca@gmail.com>
2022-06-14 20:40:37 +0100

    initial setup

---
+++ application/src/main/java/example/Application.java
@@ -0,0 +1,19 @@
+package example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class Application implements WebMvcConfigurer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+    public static void main(final String... args) {
+        SpringApplication.run(Application.class, args);
+        LOG.info("================ Application started successfully ================");
+    }
+
+}
