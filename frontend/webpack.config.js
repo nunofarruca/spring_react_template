5aaed2a02fa3208315f8034403a460d2792beff8
Nuno F <nunofarruca@gmail.com>
2022-07-13 22:09:49 +0100

    fix: Webpack Merge wasn't merging with replace of rules for scss

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,5 @@
-const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack/webpack.common.js');
+const { mergeWithRules } = require("webpack-merge");
 
 module.exports = (env, argv) => {
   const envMode = argv.mode === 'production' ? 'prod' : 'dev';
@@ -7,6 +7,12 @@ module.exports = (env, argv) => {
   console.log('\x1b[34m%s\x1b[0m', `Webpack build in mode: ${envMode}`);
 
   const envConfig = require(`./webpack/webpack.${envMode}.js`);
-  const config = merge(commonConfig, envConfig);
-  return config;
+  return mergeWithRules({
+    module: {
+      rules: {
+        test: "match",
+        use: "replace",
+      },
+    },
+  })(commonConfig, envConfig);
 };
